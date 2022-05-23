4c735b0786d75e6d0bd1673989cfdd20445ad744
danel-arratia-davinci <95108874+danel-arratia-davinci@users.noreply.github.com>
2022-05-22 22:38:36 -0300

    Add files via upload

---
+++ SoftwareTransporte1.0/src/Datos/Autoparte.java
@@ -0,0 +1,54 @@
+package Datos;
+
+
+
+public class Autoparte {
+
+String Categoria;
+String Id_producto;
+String nombreproducto;
+
+
+
+
+///CONSTRUCTOR
+
+public Autoparte(String categoria, String id_producto, String nombreproducto) {
+	super();
+	this.Categoria = categoria;
+	this.Id_producto = id_producto;
+	this.nombreproducto = nombreproducto;
+}
+
+
+///
+
+public Autoparte() {
+	super();
+}
+public String getCategoria() {
+	return Categoria;
+}
+public void setCategoria(String categoria) {
+	Categoria = categoria;
+}
+public String getId_producto() {
+	return Id_producto;
+}
+public void setId_producto(String id_producto) {
+	Id_producto = id_producto;
+}
+public String getNombreproducto() {
+	return nombreproducto;
+}
+public void setNombreproducto(String nombreproducto) {
+	this.nombreproducto = nombreproducto;
+}
+
+@Override
+public String toString() {
+	return "Autoparte [Categoria= " + Categoria + ", Id_producto=" + Id_producto + ", nombreproducto=" + nombreproducto
+			+ "]";
+}
+
+}
---
+++ SoftwareTransporte1.0/src/Negocio/Almacen.java
@@ -0,0 +1,131 @@
+package Negocio;
+
+
+import java.util.LinkedList;
+
+import Datos.Autoparte;
+import IU.Interfaz;
+
+public class Almacen{
+	
+	private int Stock;
+	private int id_Parte;
+	
+	public static void main(String[] args) {
+		Interfaz i1 = new Interfaz();
+		i1.Login();
+	}
+
+	LinkedList<Autoparte> Producto = new LinkedList<Autoparte>();
+
+	public boolean add(Autoparte autoparte) {
+		char [] letras = autoparte.getNombreproducto().toCharArray();
+		String categoria = autoparte.getCategoria();
+		letras= categoria.toCharArray();
+		String Id_prdocuto = autoparte.getId_producto();
+		letras = Id_prdocuto.toCharArray();
+		Producto.add(autoparte);
+		return true;
+	}
+	
+	public Autoparte buscar(String Id_producto) {
+		Autoparte autoparte = new Autoparte();
+		for (int i = 0; i < Producto.size(); i++) {
+			System.out.println(Producto.get(i));
+           autoparte=Producto.get(i);
+           //System.out.println(alumno.getDni());
+           //System.out.println("Dni ");
+           //System.out.println(dni);
+           String aux = autoparte.getId_producto();
+  
+           if(aux.equals(Id_producto)) {
+        	   //System.out.println("Lo encontré");
+        	   return autoparte ;        	   
+           }
+		}
+		String aux = "0";
+		autoparte.setId_producto(aux);
+		//System.out.println("no encontre");
+		return autoparte ;
+	}
+	
+	public boolean Borrar(Autoparte autoparte) {
+		
+		if(Producto.remove(autoparte)) {			
+			return true; 
+		}else {			
+			return false;
+		}
+	}
+	
+	public boolean Editar(Autoparte autoparte){
+		int index = IndexAutoparte(autoparte);
+		Producto.set(index, autoparte);
+		return true;
+	}
+	
+	private int IndexAutoparte(Autoparte autoparte) {
+		// TODO Auto-generated method stub (error en editar)
+		return 0;
+	}
+	
+	public int IndexAlumno(Autoparte autoparte){
+		Autoparte aux2 = new Autoparte();
+		for (int i = 0; i < Producto.size(); i++) {
+			System.out.println(Producto.get(i));
+           aux2=Producto.get(i);
+           String aux = aux2.getId_producto();
+           if(aux.equals(autoparte.getId_producto())) {
+        	   return i ;   
+           }
+		}
+		return 0;
+	
+	}
+	
+	public LinkedList<Autoparte> getautoparte() {
+		return Producto;
+	}
+
+	public void setUsuarios(LinkedList<Autoparte> autoparte) {
+		Producto = autoparte;
+	}
+
+	@Override
+	public String toString()	{
+		return "Listado [Productos=" + Producto + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
